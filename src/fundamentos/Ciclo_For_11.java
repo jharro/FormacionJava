28e1f47dd7fef5bfd215ea14cbf9a203047e3446
jharro <jharro@gmail.com>
2022-10-03 10:04:18 -0500

    Initial commit

--- src/fundamentos/Ciclo_For_11.java
+++ src/fundamentos/Ciclo_For_11.java
@@ -4,5 +4,8 @@ public class Ciclo_For_11 {
     public static void main(String[] args) {
         // Instrucción o sentencia for y arrays
         // http://www.edu4java.com/es/progbasica/progbasica9.html
+        for (int i=1;i<=5;i++){
+            System.out.println("Sapo lambónn");
+        }
     }
 }
